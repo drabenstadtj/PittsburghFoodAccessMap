913ad2934d44424ff33dc7e7132d2f8407ec5b27
Cole Swierczek <swierczekcj@gmail.com>
2025-10-22 11:16:21 -0400

    testing with sample markers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import './dist/leaflet.css'
+import L from 'leaflet';
+import sampleData from './samplegeo.json';
+import markerIcon from './dist/images/marker-icon.png';
+import markerShadow from './dist/images/marker-shadow.png'
+
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 
 
+//Had to do this weird reset I found on stack overflow in order to get the marker icons
+//to appear - some quirk where react-leaflet doesn't include images and URLs must be manually reset
+let DefaultIcon = L.icon({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
 function App() {
+  const collect = sampleData.features;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[40.4418, -79.972]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-    </MapContainer>
+        {collect.map((p) => 
+          <Marker position = {p.geometry.coordinates}>
+            <Popup>
+              {p.properties.name}
+            </Popup>
+          </Marker>
+        )}
+      </MapContainer>
     </div>
   );
 }
